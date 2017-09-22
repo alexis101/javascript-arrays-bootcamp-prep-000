f11b0ad78ba2766ebc9ac995f5930ce8e5124285
alexis101 <kondilisalexis@gmail.com>
2017-09-22 21:52:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,13 +19,16 @@ function accessElementInArray(chocolateBars, index){
   return chocolateBars[2];
 }
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.shift();
+  chocolateBars.shift();
+  return chocolateBars
+  
 }
 function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-  return chocolateBars.pop();
+  chocolateBars.pop();
+  return chocolateBars
 }
 function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length -1);
